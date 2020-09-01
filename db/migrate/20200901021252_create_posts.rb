376a6677a603f9ade7a156778a3419d02b101df6
max3252 <68981701+max3252@users.noreply.github.com>
2020-09-01 11:25:25 +0900

    postsテーブル作成

---
+++ db/migrate/20200901021252_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :image,             null: false
+      t.string :name,              null: false
+      t.integer :genre_id,         null: false
+      t.text :allergies,           null: false
+      t.string :store_name,        null: false
+      t.integer :price,            null: false
+      t.string :address          
+      t.text :text,                null: false
+      t.references :user,          foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_034049) do
+ActiveRecord::Schema.define(version: 2020_09_01_021252) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.string "name", null: false
+    t.integer "genre_id", null: false
+    t.text "allergies", null: false
+    t.string "store_name", null: false
+    t.integer "price", null: false
+    t.string "address"
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_08_31_034049) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
