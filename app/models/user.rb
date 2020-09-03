b53bebdaa97fd094091948314ded7457c57ffca2
max3252 <68981701+max3252@users.noreply.github.com>
2020-09-03 15:29:19 +0900

    新規ユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,18 @@ class User < ApplicationRecord
   belongs_to_active_hash :prefecture 
   belongs_to_active_hash :gender
   belongs_to_active_hash :age
-  
+
   with_options numericality: { other_than: 1, message: 'Select' } do
     validates :prefecture_id
     validates :gender_id
     validates :age_id
   end
 
-  validates :nickname,
-            presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :allergies
+  end
+  
   validates :password,
             confirmation: true,
             format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
