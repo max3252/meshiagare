57c81176c89f840acc7f6a2dfa00dd63d61f4f0d
max3252 <68981701+max3252@users.noreply.github.com>
2020-09-04 19:39:39 +0900

    投稿機能の実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,3 +1,17 @@
-$(function() {
-  $('.slider').slick();
-  });
+window.addEventListener('load', function(){
+const pics_src = ["images/meshiagare.jpg","images/meshiagare.top03.png","images/meshiagare.top02.jpg"];
+let num = -1;
+ 
+ 
+function slideshow_timer(){
+  if (num === 2){
+    num = 0;
+  } 
+  else {
+    num ++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+ 
+setInterval(slideshow_timer, 5000);
+})
