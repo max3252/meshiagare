ec938592931d20952c1c147ff719d7db93c1aada
max3252 <68981701+max3252@users.noreply.github.com>
2020-09-14 09:43:47 +0900

    チャット機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,5 +6,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(text: params[:message][:text], user_id: current_user.id)
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message,username: @message.user.nickname
+    end
   end
 end
